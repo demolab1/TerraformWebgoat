560dc9102db128987713a9851b853d3083129390
ComplianceLab <116727839+ComplianceLab@users.noreply.github.com>
2024-02-28 14:20:09 +0000

    Update ec2.tf

--- terraform/aws/ec2.tf
+++ terraform/aws/ec2.tf
@@ -24,9 +24,15 @@ echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
 
 
 
+
+
+
+
+
+
 /* default secrets in this file */
 export AWS_ACCESS_KEY_ID=AKIAIOSFODNN7EXAMAAA
-export AWS_SECRET_ACCESS_KEY=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
+export AWS_SECRET_ACCESS_KEY=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMAAAKEYSS
 
 
 
